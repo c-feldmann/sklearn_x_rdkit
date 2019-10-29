8080a136a11a45f09b55092759c64d366ad7189b
c-feldmann <cfeldmann@bit.uni-bonn.de>
2019-10-30 00:28:27 +0100

    Made it work somewhat

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup
 setup(
     name='sklearn_x_rdkit',
     version='0.0.1',
-    packages=[''],
-    url='',
-    license='',
     author='Christian Feldmann',
+    license="BSD",
+    packages=['sklearn_x_rdkit', 'tests'],
     author_email='cfeldmann@bit.uni-bonn.de',
-    description='Convenient transfer from rdkit features to sklearn input', install_requires=['scipy',
-                                                                                              'bidict', 'numpy']
+    description='Convenient transfer from rdkit features to sklearn input',
+    install_requires=['scipy', 'bidict', 'numpy']
 )
