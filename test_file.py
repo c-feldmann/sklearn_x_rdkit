b2c667fd969f462f1f931aaea0072569815e0359
c-feldmann <cfeldmann@bit.uni-bonn.de>
2019-10-29 22:16:28 +0100

    Adding/Updating test-cases

--- test_file.py
+++ test_file.py
@@ -1,6 +1,11 @@
 import unittest
 
+import numpy as np
+import scipy.sparse as sparse
+
 from fingerprints import UnfoldedMorganFingerprint
+from kernel import tanimoto_from_sparse
+from supporting_functions import construct_check_mol_list
 
 # noinspection SpellCheckingInspection
 smiles_list = ["c1ccccc1",
@@ -15,12 +20,43 @@ smiles_list = ["c1ccccc1",
 
 class ConstructingFingerprints(unittest.TestCase):
     def test_independence_of_constructing(self):
+        mol_obj_list = construct_check_mol_list(smiles_list)
         ecfp2_1 = UnfoldedMorganFingerprint()
-        fp1 = ecfp2_1.fit_transform(smiles_list)
+        fp1 = ecfp2_1.fit_transform(mol_obj_list)
         ecfp2_2 = UnfoldedMorganFingerprint()
-        ecfp2_2.fit(smiles_list)
-        fp2 = ecfp2_2.transform(smiles_list)
+        ecfp2_2.fit(mol_obj_list)
+        fp2 = ecfp2_2.transform(mol_obj_list)
         self.assertTrue((fp1 != fp2).nnz == 0)
+
+
+class Kernel(unittest.TestCase):
+    def test_kernel_simple_vectors(self):
+        test_fingerprint1 = sparse.csr_matrix(np.array([[0, 0, 0, 1],
+                                                        [0, 0, 1, 1],
+                                                        [0, 1, 0, 0]]
+                                                       )
+                                              )
+        test_fingerprint2 = sparse.csr_matrix(np.array([[0, 0, 0, 1],
+                                                        [0, 0, 1, 1],
+                                                        [0, 1, 1, 0],
+                                                        [1, 0, 0, 0],
+                                                        ]
+                                                       )
+                                              )
+        expected_matrix = np.array([[1, 0.5, 0, 0],
+                                    [0.5, 1, 1 / 3, 0],
+                                    [0, 0, 0.5, 0]
+                                    ])
+        self.assertTrue(np.all(np.isclose(tanimoto_from_sparse(test_fingerprint1, test_fingerprint2), expected_matrix)))
+
+    def test_real_fp_as_input(self):
+        mol_obj_list = construct_check_mol_list(smiles_list)
+        ecfp2_1 = UnfoldedMorganFingerprint()
+        fp1 = ecfp2_1.fit_transform(mol_obj_list)
+        sim_matrix = tanimoto_from_sparse(fp1, fp1)
+        self.assertEqual(sim_matrix.shape[0], sim_matrix.shape[1])
+        self.assertEqual(sim_matrix.shape[0], len(mol_obj_list))
+        self.assertTrue(np.all(np.isclose(sim_matrix.diagonal(), np.ones((len(mol_obj_list), 1)))))
 
 
 if __name__ == '__main__':
