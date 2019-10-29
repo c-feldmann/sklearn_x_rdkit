5cad9251e4744ccc4ab8fb2c94612b18d6ff6a63
c-feldmann <cfeldmann@bit.uni-bonn.de>
2019-10-29 21:52:53 +0100

    Adding Comments

--- kernel.py
+++ kernel.py
@@ -3,6 +3,9 @@ import scipy.sparse as sparse
 
 
 def tanimoto_from_sparse(fp_matrix1: sparse.csr_matrix, fp_matrix2: sparse.csr_matrix) -> np.matrix:
+    """Calculates the Tanimoto-similarity
+    :returns a matrix with a pairwise comparison of vectors in fingerprint 1 with vectors of fingerprint 2
+    """
     intersection = fp_matrix1.dot(fp_matrix2.transpose())
     sum_fp1 = fp_matrix1.sum(axis=1).dot(np.ones((1, fp_matrix2.shape[0])))
     sum_fp2 = fp_matrix2.sum(axis=1).dot(np.ones((1, fp_matrix1.shape[0]))).transpose()
