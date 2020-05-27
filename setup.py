507cfa159bd02e9901a7520ce578c18d1afa93d7
c-feldmann <cfeldmann@bit.uni-bonn.de>
2020-05-27 17:19:49 +0200

    Updating Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sklearn_x_rdkit',
-    version='0.0.1.1',
+    version='0.0.1.2',
     author='Christian Feldmann',
     license="BSD",
     packages=['sklearn_x_rdkit', 'tests'],
